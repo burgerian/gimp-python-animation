c5efea520ed1af03b832fb9e429aaf6750740fe2
Ant <ant.h@gmx.net>
2020-07-27 22:13:53 +0100

    Change to a GIMP plugin

--- README.md
+++ README.md
@@ -4,31 +4,38 @@
 
 ## Introduction
 
-GIMP is a good tool for animation, but it can be quite laborious to use it, involving a lot of repetitive clicks.  This project contains code written for GIMP's python-fu feature that helps to speed up various animation tasks.
+GIMP is a good tool for animation, but it can be quite laborious to use it, involving a lot of repetitive clicks.  This plugin adds more commands to GIMP's Animation menu that help to speed up various animation tasks.
+
+Author: Anthony Hayward.
 
 ## Compatibility
 
-Tested with GIMP 2.10.12 on Windows.  No plugins or extra software needed.
+Tested with GIMP 2.10.12 on Windows.  No other plugins or extra software needed.
 
 GIMP = GNU Image Manipulation Program, an open source graphics editor, available for free download at https://www.gimp.org/downloads/.
 
-## GIMP Setup
+## Installation
 
-The process assumes you don't have any other images open in GIMP apart from the images related to the animation, so close anything else you have open before starting.
+1. Find the plugin directory for your user by clicking Edit > Preferences > Folders > Plug-ins and click the icon to "Show file location in the file manager"
+2. Copy [python_animation_helper_plugin.py](python_animation_helper_plugin.py) into the directory.
+3. Restart GIMP.
 
-You will need the following GIMP dialogs open all the time:
+If GIMP is happy, you'll see new entries in the Filters > Animation menu:
 
-- Filters > Python-fu > Console (a separate window from the main GIMP window)
-- Windows > Dockable Dialogs > Undo History (a dialog shown within the main GIMP window)
-- Windows > Dockable Dialogs > Layers (a dialog shown within the main GIMP window)
+![GIMP menu with extra animation commands](animation_menu.png)
 
-Copy/paste the code from [animation.py](animation.py) into the Python-fu console.  This will define functions, but not run them, so it won't make any changes to your images yet.  Keep the Python-fu console open throughout the whole process - you can switch between the console and the main GIMP window as required.  If you close it by mistake, just open it again and copy/paste [animation.py](animation.py) again.
+## GIMP Dialogs
 
-If you don't like the result of any script operation, use GIMP's Undo History and click above the list of changes made by the script to reverse its effect.
+You will need the following GIMP dialogs from the "Windows > Dockable Dialogs" menu open all the time in the main GIMP window:
+
+- Undo History
+- Layers
+
+If you don't like the result of any animation operation, use GIMP's Undo History and click above the list of changes made by the script to reverse its effect.
 
 ## Process
 
-These scripts are designed to help get animations that have been drawn on multiple bits of paper into GIMP as multiple layers within a single image, from where they can be exported as an animated GIF, or as separate images or a sprite grid for use in a game.
+These commands are designed to help get animations that have been drawn on multiple bits of paper into GIMP as multiple layers within a single image, from where they can be exported as an animated GIF, or as separate images or a sprite grid for use in a game.
 
 Of course, you can adapt this process and modify your copy of the Python code to suit your specific needs.
 
@@ -36,20 +43,26 @@ Of course, you can adapt this process and modify your copy of the Python code to
 
 1. Draw animation frames on paper.  Write a frame number on each frame so you can keep track of them.
 2. Scan each piece of paper into GIMP using "File > Create > From Scanner/Camera..."  (as a colour picture).  Try to have the orientation the same for each piece of paper; it doesn't have to be correct, just the same for each image at this point.
-3. Run `stack()` in the Python-fu console.  All the images get combined into one image as layers.
+3. Run "Filters > Animation > Stack all images onto this one".  All the images get combined into one image as layers.
 4. Use "Image > Transform" to rotate the image to the correct orientation.
 5. Use "Image > Scale" to scale the image to the pixel dimensions you want.  It's best if the image fits on your screen at 100% zoom.
 6. Save the image (in GIMP's `.xcf` format).
 7. File > Close All and discard changes.  This is necessary because the Python-fu can't close your other now-empty images for you.
-8. Reopen the GIMP file you saved.
+8. Reopen the GIMP file you just saved.
 
 ### Colour and outline
 
-The scanned images often have poor contrast.  You can run the script `bc()` to apply Contrast + 50% to all layers.  You might want more sophisticated enhancement, e.g. apply a colour curve or GIMP's Cartoon filter (Filters > Artistic > Cartoon...) which can ink your outlines for you.  Unfortunately you'll need to apply these manually to each layer separately.  You can save colour curve presets, and make a note of Cartoon settings, to make sure you do it consistently.
+The scanned images often have poor contrast.  You can adjust the brightness / contrast of all layers simultaneously using "Filters > Animation > Brightness/contrast on all layers".
+
+You might want more sophisticated enhancement, e.g. apply a colour curve or GIMP's Cartoon filter (Filters > Artistic > Cartoon...) which can ink your outlines for you.  Unfortunately you'll need to apply these manually to each layer separately.  You can use "Filters > Repeat" and "Filters > Recently Used" to apply the same enhancement after switching to the next layer.
 
 If you have only drawn outlines, this is the time to colour in using GIMP's Flood Fill tool.
 
-It's very easy to get confused between the top visible layer and the currently active (selected) layer.  You can run the Python `f()` function to select the top visible layer and hide the rest, and `up()` and `down()` to move up and down the layer stack.
+It's very easy to get confused between the top visible layer and the currently active (selected) layer.  The plugin provides some "Solo layer" commands to help:
+- Solo layer up
+- Solo layer down
+- Solo layer: top visible
+These commands hide all but one layer, and ensure the displayed layer is selected.
 
 ### Split and align layers if required
 
@@ -58,14 +71,14 @@ If you have multiple frames drawn on each sheet of paper, it's now time to split
 1. Right-click on each layer in the Layers dialog and select "Duplicate Layer" until you have as many copies of the layer as there are frames in the layer.
 2. Rename each layer with the frame number that the layer will represent.  E.g. if you have three copies of a layer that contains frames 1, 2 and 3, then you will end up with three copies of that layer called "1", "2" and "3".  To rename a layer, click it in the Layer dialog and press F2.
 3. Create guide lines (by dragging down and across from the ruler) that you can use to align your frames.  Turn off "Snap to Guides" from the View menu.
-4. Go through each layer and use the Move tool to shift the whole layer to line up the frames.  To select a single layer, run the Python function `f(1)` to activate layer 1, `f(2)` to activate layer 2, etc. - this also hides all the other layers, to avoid confusion.  You can also use `up()` and `down()` to move to the next layer.
+4. Go through each layer and use the Move tool to shift the whole layer to line up the frames.  To select a single layer, use one of the "Filters > Animation > Solo layer *" commands.
 5. Select a rectangle that contains all frame contents, and the Image > Crop to Selection.
-6. Go through each layer (using the `down()` function) and delete any mess around the edges, e.g. parts of other frames, using the usual GIMP drawing / selection tools.
+6. Go through each layer (using the  "Filters > Animation > Solo layer down" command) and delete any mess around the edges, e.g. parts of other frames, using the usual GIMP drawing / selection tools.
 
 ### Sort frames
 
 1. If you haven't already numbered the layers, rename each layer to have the frame number "1", "2" etc.  To rename a layer, click it in the Layer dialog (Windows > Dockable Dialogs > Layers) and press F2.
-2. In the Python-fu console, run `sort()` to sort the frames from bottom to top.  Note: this also sets the layer size to the image size for each layer.
+2. In the Python-fu console, run "Filters > Animation > Sort layers" to sort the frames from bottom to top.  Note: this also sets the layer size to the image size for each layer.
 
 ### Preview animation
 
@@ -75,13 +88,11 @@ If you have multiple frames drawn on each sheet of paper, it's now time to split
 4. Change the "1x" option to see your animation at a different playback rate.  (You can change the "fps" option as well, but you need to close and re-open the dialog box for it to take effect).
 5. Close the dialog when done.
 
-### Save animation as animated GIF
+### Export animation as animated GIF
 
-You can run the function `gif(ms)` to save the animation as an animated GIF file.  Replace `ms` with the number of milliseconds to show each frame.
+You can run the command "Filters > Animation > Export as animated GIF" to save the animation as an animated GIF file.  Enter the desired frame rate and optionally a suffix for the filename.
 
-To convert frames per second to milliseconds delay, divide 1000 by the quantity, or use this quick guide: 10 fps = 100 ms, 12.5 fps = 80 ms, 15 fps = 67 ms, 20 fps = 50 ms, 25 fps = 40 ms.
-
-If you prefer more control or the Python script doesn't work, you can convert to a GIF manually as follows.
+If you prefer to specify frame delay rather than frame rate, you can export an animated GIF using the File menu instead:
 
 1. Click File > Export As...
 2. Enter a filename ending in `.gif` and click Export.
@@ -96,9 +107,9 @@ GIMP can't save movie files, so you'll need other software if you want to conver
 
 ### Save animation as separate frames
 
-Run `png()` function.
+"Filters > Animation > Export each frame as PNG"
 
-You can also specify a path and filename root e.g. `png(r"C:\Users\me\Pictures\my_anim")` will save the layers as `my_anim_01.png`, `my_anim_02.png`, `my_anim_03.png` etc.
+Each frame is saved to a PNG file with its frame number as a suffix, e.g. `my_anim_01.png`, `my_anim_02.png`, `my_anim_03.png` etc.
 
 ### Save animation as sprite map
 
@@ -106,19 +117,19 @@ TODO
 
 ### Remove box around frame (transparent edge)
 
-Your scans will be full white rectangles with no transparency.  To add an Alpha Channel to your frames and delete the white edge around the outside, run Python function `nobox()`.
+Your scans will be full white rectangles with no transparency.  To add an Alpha Channel to your frames and delete the white edge around the outside, run "Filters > Animation > Remove white box".
 
-After removing the frame, run the Python function `showall()` to see all the frames overlaid on each other, then you can easily crop the image to a box that includes all the content in all the frames.
+After removing the outer boxing, run the command "Filters > Animation > Show all layers" to see all the frames overlaid on each other, then you can easily crop the image to a box that includes all the content in all the frames.
 
 
 ### Mirror the frames (reverse loop playback)
 
-GIMP doesn't offer to reverse your animation when it's finished playing.  You can do this by copying each frame in reverse order, except the last one.  The Python function to do this is `mirror()`.
+GIMP doesn't offer to reverse your animation when it's finished playing.  You can do this by copying each frame in reverse order, except the last one.  Run "Filters > Animation > Repeat frames backwards".
 
 Note, the copied frame has no relationship to the original frame, so make sure any other editing is completed before doing this!  The copy gets a new incremental frame number that does not show its relationship to the original frame number.
 
 ### Numbering
 
-The `number()` function will assign an incremental numeric layer name to any layer that has a non-numeric name, while leaving layers that already have numeric names alone.  If there are multiple layers, they are numbered increasing from bottom to top.
+The "Filters > Animation > Number unnumbered layers" command will assign an incremental numeric layer name to any layer that has a non-numeric name, while leaving layers that already have numeric names alone.  If there are multiple layers, they are numbered increasing from bottom to top.
 
-The `renumber()` function will rename all layers, starting from "1" at the bottom, then "2" etc.  Existing layer names are ignored.
+The "Filters > Animation > Renumber all layers" command will rename all layers, starting from "1" at the bottom, then "2" etc.  Existing layer names are ignored.
---
+++ python_animation_helper_plugin.py
@@ -0,0 +1,489 @@
+#!/usr/bin/env python
+import gimpfu
+from gimpfu import (
+    gimp, pdb
+)
+
+
+def stack(target=None):
+    for image in gimp.image_list():
+        pdb.gimp_selection_none(image)
+        if image.layers:
+            if not target:
+                target = image
+            elif image != target:
+                for layer in image.layers:
+                    pdb.gimp_edit_cut(layer)
+                    floating = pdb.gimp_edit_paste(target.layers[0], True)
+                    pdb.gimp_floating_sel_to_layer(floating)
+
+
+def bc(image=None, brightness=0, contrast=50):
+    if not image:
+        image = gimp.image_list()[0] 
+    for layer in image.layers:
+        pdb.gimp_drawable_brightness_contrast(layer, brightness / 100.0, contrast / 100.0)
+
+
+def nobox(image=None):
+    if not image:
+        image = gimp.image_list()[0] 
+    for layer in image.layers:
+        layer.add_alpha()
+        (w, h) = (image.width - 1, image.height - 1)
+        for (x, y) in [(0, 0), (w, 0), (0, h), (w, h)]:
+            pdb.gimp_image_select_contiguous_color(image, 2, layer, x, y)
+        pdb.gimp_edit_clear(layer)
+
+
+def sort(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    for layer in image.layers:
+        pdb.gimp_layer_resize_to_image_size(layer)
+    more = True
+    while more:
+        more = False
+        for a, b in zip(image.layers, image.layers[1:]):
+            move_down = False
+            try:
+                move_down = int(a.name) < int(b.name)
+            except ValueError:
+                move_down = a.name < b.name
+            if move_down:
+                pdb.gimp_image_lower_item(image, a)
+                more = True
+                break
+
+
+def number(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    highest = 0
+    unnumbered = 0
+    for layer in image.layers:
+        try:
+            n = int(layer.name)
+            if n > highest:
+                highest = n
+        except ValueError:
+            unnumbered += 1
+    v = highest + unnumbered
+    for layer in image.layers:
+        try:
+            int(layer.name)
+        except ValueError:
+            layer.name = str(v)
+            v -= 1
+
+
+def renumber(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    for (layer_index, layer) in enumerate(image.layers):
+        layer.name = str(len(image.layers) - layer_index)
+
+
+def frame(image=None, name=None):
+    if not image:
+        image = gimp.image_list()[0]
+    if isinstance(name, int):
+        name = str(name)
+    if name:
+        for layer in image.layers:
+            if layer.name == name:
+                layer.visible = True
+                pdb.gimp_image_set_active_layer(image, layer)
+            else:
+                layer.visible = False
+    else:
+        first = True
+        for (layer_index, layer) in enumerate(image.layers):
+            if first and (layer.visible or layer_index == len(image.layers) - 1):
+                layer.visible = True
+                pdb.gimp_image_set_active_layer(image, layer)
+                first = False
+            else:
+                layer.visible = False
+
+f = frame
+
+
+def up(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    if not image.layers:
+        return
+    selected = False
+    select_layer = None
+    for layer in image.layers:
+        if layer.visible:
+            if not selected and select_layer:
+                select_layer.visible = True
+                pdb.gimp_image_set_active_layer(image, select_layer)
+                selected = True
+            layer.visible = False
+        else:
+            layer.visible = False
+            if not selected:
+                select_layer = layer
+    if not selected:
+        select_layer.visible = True
+        pdb.gimp_image_set_active_layer(image, select_layer)
+
+
+def down(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    if not image.layers:
+        return
+    select_next = False
+    selected = False
+    for layer in image.layers:
+        if select_next and not selected:
+            layer.visible = True
+            pdb.gimp_image_set_active_layer(image, layer)
+            selected = True
+        elif layer.visible:
+            select_next = True
+            layer.visible = False
+    if not selected:
+        layer = image.layers[0]
+        layer.visible = True
+        pdb.gimp_image_set_active_layer(image, layer)
+
+
+def showall(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    for layer in image.layers:
+        layer.visible = True
+
+
+def mirror(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    max_layer = 0
+    for layer in image.layers:
+        try:
+            if int(layer.name) > max_layer:
+                max_layer = int(layer.name)
+        except ValueError:
+            pass
+    layers_to_reverse = image.layers[1:]
+    pdb.gimp_selection_none(image)
+    for layer in layers_to_reverse:
+        pdb.gimp_edit_copy(layer)
+        floating = pdb.gimp_edit_paste(image.layers[0], True)
+        pdb.gimp_floating_sel_to_layer(floating)
+        max_layer += 1
+        floating.name = str(max_layer)
+
+
+def png(image=None):
+    if not image:
+        image = gimp.image_list()[0]
+    prefix = pdb.gimp_image_get_filename(image)[:-4] + "_"
+    gimp.progress_init("Save frames as {}_*.png".format(prefix))
+    for (layer_index, layer) in enumerate(image.layers):
+        try:
+            filename = "{}{:02d}.png".format(prefix, int(layer.name))
+        except ValueError:
+            filename = "{}{}.png".format(prefix, layer.name)
+        pdb.file_png_save(
+            image, layer,
+            filename, None,
+            True, # interlace
+            9, # compression
+            True, # bkgd
+            True, # gama
+            True, # offs
+            True, # phys
+            True, # time
+        )
+        gimp.progress_update(100 * (layer_index + 1) / len(image.layers))
+    gimp.message("All frames saved as {}_*.png".format(prefix))
+
+
+def gif(image=None, suffix=None, fps=24):
+    if not image:
+        image = gimp.image_list()[0]
+    file_path = pdb.gimp_image_get_filename(image)[:-4]
+    if suffix:
+        file_path += "_" + suffix.strip()
+    file_path += ".gif"
+    ms = int(1000.0 / fps)
+    temp_image = False
+    try:
+        gimp.progress_init("Save animated GIF @ {} fps = {} ms/frame as {}".format(
+            fps, ms, file_path))
+        if pdb.gimp_image_base_type(image) != 2:
+            temp_image = True
+            image = pdb.gimp_image_duplicate(image)
+            pdb.gimp_image_convert_indexed(
+                image,
+                0, # dither-type=CONVERT-DITHER-NONE
+                0, # palette-type=CONVERT-PALETTE-GENERATE
+                255, # num-cols
+                False, # alpha-dither
+                False, # remove-unused
+                "" # palette
+            )
+            gimp.progress_update(50)
+        pdb.file_gif_save(
+            image, image.layers[0],
+            file_path, file_path,
+            True, # interlace
+            True, # loop
+            ms, # default-delay
+            2 # default-dispose
+        )
+        gimp.progress_update(100)
+        gimp.message("Saved animated GIF @ {} fps = {} ms/frame as {}".format(
+            fps, ms, file_path))
+    finally:
+        if temp_image:
+            pdb.gimp_image_delete(image)
+
+
+gimpfu.register(
+    "python_animation_helper_stack",
+    "Stack all layers from all other images onto the current image",
+    "Plugin to stack all layers from all other images onto the current image.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Stack all images onto this one",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "target", "Image to modify", None), # type, name, description, default
+    ],
+    [],
+    stack,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_bc",
+    r"Adjust brightness and contrast on all layers.",
+    "Plugin to increase contrast on all layers of the current image.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    r"Brightness/contrast on all layers",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image to modify", None), # type, name, description, default
+		(gimpfu.PF_SLIDER, "brightness", "Brightness", 0, (-50, 50, 1)),
+		(gimpfu.PF_SLIDER, "contrast", "Contrast", 50, (-50, 50, 1)),
+	],
+	# output/return values
+    [],
+    bc,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_nobox",
+    "Add alpha channel to all layers and remove bounding white box",
+    "Plugin to add alpha channel to all layers and remove bounding white box in the current image.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Remove white box",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image to modify", None), # type, name, description, default
+    ],
+    [],
+    nobox,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_sort",
+    "Set all layers to image size and sort by name",
+    "Plugin to set all layers to image size and sort by name.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Sort layers",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image to modify", None), # type, name, description, default
+    ],
+    [],
+    sort,
+    menu="<Image>/Filters/Animation",
+)
+
+
+gimpfu.register(
+    "python_animation_helper_number",
+    "Number unnumbered frames, bottom to top",
+    "Plugin to number unnumbered frames, bottom to top.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Number unnumbered layers",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image to modify", None), # type, name, description, default
+    ],
+    [],
+    number,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_renumber",
+    "Number all frames, bottom to top",
+    "Plugin to number all frames, bottom to top.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Renumber all layers",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image to modify", None), # type, name, description, default
+    ],
+    [],
+    renumber,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_frame",
+    "Focus on top visible frame",
+    "Plugin to focus on top visible frame.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Solo layer: top visible",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+    ],
+    [],
+    frame,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_up",
+    "Focus on previous frame",
+    "Plugin to focus on previous frame.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    r"Solo layer /\ /\ /\ up",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+    ],
+    [],
+    up,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_down",
+    "Focus on previous frame",
+    "Plugin to focus on previous frame.",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    r"Solo layer \/ \/ \/ down",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+    ],
+    [],
+    down,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_showall",
+    "Show all layers (piled up)",
+    "Plugin to show all layers",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Show all layers",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+    ],
+    [],
+    showall,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_mirror",
+    "Repeat frames in reverse order (mirror)",
+    "Plugin to repeat frames in reverse order (mirror)",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Repeat frames backwards",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+    ],
+    [],
+    mirror,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_gif",
+    "Save as animated GIF",
+    "Plugin to save as animated GIF",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Export as animated GIF",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+        (gimpfu.PF_STRING, "suffix", "Suffix (to append to .xcf filename)", ""),
+        (gimpfu.PF_SPINNER, "fps", "Frames per second", 24, (1, 60, 1)),
+    ],
+    [],
+    gif,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.register(
+    "python_animation_helper_png",
+    "Save frames as PNG images",
+    "Plugin to save frames as PNG images",
+    "Anthony Hayward",
+    "Anthony Hayward 2020.  LGPL License",
+    "2020",
+    "Export each frame as PNG",
+    "*",
+	# input parameters. Same count and order as for plugin_func parameters 
+    [
+		(gimpfu.PF_IMAGE, "image", "Image", None), # type, name, description, default
+    ],
+    [],
+    png,
+    menu="<Image>/Filters/Animation",
+)
+
+gimpfu.main()
